52caf01461f089d2abdf2037564ab3d302559786
Arrasty <farahgesty@gmail.com>
2024-06-02 15:45:01 +0700

    first commit

---
+++ app/src/main/java/com/capstone/nutritrack/customview/MyEditText.kt
@@ -0,0 +1,41 @@
+package com.capstone.nutritrack.customview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+
+class MyEditText @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : AppCompatEditText(context, attrs) {
+
+    init {
+        addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.toString().length < 8) {
+                    setError("Password tidak boleh kurang dari 8 karakter", null)
+                } else {
+                    error = null
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        hint = "Masukkan password Anda"
+        textAlignment = View.TEXT_ALIGNMENT_VIEW_START
+    }
+}
